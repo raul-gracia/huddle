d65c678762e651a2c024669c849b033d923282e7
Raul Gracia <raulgracialario@gmail.com>
2017-06-26 03:17:54 +0100

    Adding devise to have authenticated users

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 gem 'font-awesome-rails', '~> 4.7', '>= 4.7.0.2'
 gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
 
+gem 'devise', '~> 4.3'
+
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe User do
+  describe 'has db structure' do
+    it { should have_db_column(:name) }
+    it { should have_db_column(:email) }
+    it { should have_db_column(:encrypted_password) }
+  end
+
+  describe 'has validations' do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:name) }
+  end
+
+  describe 'has relationships' do
+    #it { should have_many(:companies) }
+    #it { should have_many(:contacts).through(:companies) }
+    #it { should have_many(:activities).through(:contacts) }
+  end
+end
